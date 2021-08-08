f5d3569716b618c4fb1b4a0e0d9ae79bbfae0977
Dominik <admin@dominik48n.de>
2021-08-09 01:30:44 +0200

    Create check string hash method

--- stringhashapi.py
+++ stringhashapi.py
@@ -11,3 +11,14 @@ def create_string_hash(string: str):
         bcrypt.gensalt(12)
     )
     return hashed.decode()
+
+
+def check_string_hash(string: str, hash_value: str):
+    string = string.encode("utf-8")
+    string = base64.b64encode(hashlib.sha256(string).digest())
+    hash_value = hash_value.encode("utf-8")
+
+    if bcrypt.checkpw(string, hash_value):
+        return True
+    else:
+        return False
