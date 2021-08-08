80df3de85b552849df7e97a37921f5979f64b36d
Dominik <admin@dominik48n.de>
2021-08-09 01:27:36 +0200

    Implement base64

--- stringhashapi.py
+++ stringhashapi.py
@@ -1,8 +1,11 @@
+import base64
+import hashlib
 import bcrypt
 
 
 def create_string_hash(string: str):
     string = string.encode("utf-8")
+    string = base64.b64encode(hashlib.sha256(string).digest())
     hashed = bcrypt.hashpw(
         string,
         bcrypt.gensalt(12)
